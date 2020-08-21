ebb3038a4ef31c5b9f21b610c8b7a9e2a67be3a3
Adam Habil <ahabil@outlook.com>
2020-08-21 01:30:48 -0600

    Adding source code of the bot
    Adding source code of the bot

---
+++ Modules/Commands.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace TutorialBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
+
+        [Command("kick")]
+        [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "You cannot kick this user because you do not have the required server permission.")]
+        public async Task KickMember(IGuildUser user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("Please specify a user!");
+                return;
+            }
+            if (reason == null) reason = "Not specified";
+
+            await user.KickAsync(reason);
+
+            var EmbedBuilder = new EmbedBuilder()
+                .WithDescription($":white_check_mark: {user.Mention} was kicked\n**Reason: ** {reason}");
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+        }
+
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You cannot ban this user because you do not have the required server permission.")]
+        public async Task BanMember(IGuildUser user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("Please specify a user!");
+                return;
+            }
+            if (reason == null) reason = "Not specified";
+
+            await Context.Guild.AddBanAsync(user, 1, reason);
+
+            var EmbedBuilder = new EmbedBuilder()
+                .WithDescription($":white_check_mark: {user.Mention} was banned\n**Reason: ** {reason}")
+                .WithFooter(footer =>
+                {
+                    footer
+                    .WithText("User Ban Log")
+                    .WithIconUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS53Jm7dqFTla_I3zpevb9fc-a-iQ1T96V3Vw&usqp=CAU");
+                });
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+
+            ITextChannel logChannel = Context.Client.GetChannel(746235465442525295) as ITextChannel;
+            var EmbedBuilderLog = new EmbedBuilder()
+                .WithDescription($"{user.Mention} was banned\n**Reason: ** {reason}\n**Moderator: ** {Context.User.Mention}")
+                .WithFooter(footer =>
+                {
+                    footer
+                    .WithText("User Ban Log")
+                    .WithIconUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS53Jm7dqFTla_I3zpevb9fc-a-iQ1T96V3Vw&usqp=CAU");
+                });
+            Embed embedLog = EmbedBuilderLog.Build();
+            await logChannel.SendMessageAsync(embed: embedLog);
+        }
+
+        [Command("unban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You cannot ban this user because you do not have the required server permission.")]
+        public async Task UnbanMember(IGuildUser user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("Please specify a user!");
+                return;
+            }
+            if (reason == null) reason = "Not specified";
+            
+            await Context.Guild.RemoveBanAsync(user);
+
+            var EmbedBuilder = new EmbedBuilder()
+                .WithDescription($":white_check_mark: {user.Mention} was unbanned\n");
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
+
